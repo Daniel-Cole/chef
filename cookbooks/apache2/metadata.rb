9b486ae975be3545524db1e67264760add1bda58
Daniel Cole <daniel.cole@superchoice.com.au>
2017-08-02 21:38:46 +1000

    added metadata for apache2 recipe

--- cookbooks/apache2/metadata.rb
+++ cookbooks/apache2/metadata.rb
@@ -1,4 +1,4 @@
-name 'output'
+name 'apache2'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
--- cookbooks/apache2/recipes/default.rb
+++ cookbooks/apache2/recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook:: output
+# Cookbook::apache2
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
