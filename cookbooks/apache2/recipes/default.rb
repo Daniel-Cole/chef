0d71e733b4e1d89f8d25ab59a3d9221598744c44
Daniel Cole <daniel.cole@superchoice.com.au>
2017-08-02 20:47:53 +1000

    add apache recipe

---
+++ cookbooks/apache2/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook:: output
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+package 'Install Apache' do
+  case node[:platform]
+  when 'redhat', 'centos'
+    package_name 'httpd'
+  when 'ubuntu', 'debian'
+    package_name 'apache2'
+  end
+end
